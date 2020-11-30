3de8a6717e4bd34c5545f71e868a56ef11b33a1b
Napalm <69393030+dlwnghks6821@users.noreply.github.com>
2020-11-30 10:56:15 +0900

    수박수박

---
+++ 1단계 수박수박.java
@@ -0,0 +1,22 @@
+package newnew1;
+import java.util.Scanner;
+
+public class new01 {
+
+	public static void main(String[] args) {
+		//길이가 n이고, 수박수박수박수....와 같은 패턴을 유지하는 문자열을 리턴하는 함수,
+		//solution을 완성하세요. 예를들어 n이 4이면
+		//수박수박을 리턴하고 3이라면 수박수를 리턴하면 됩니다.
+		String n = "수박수박수박수박수박수박수박수박";
+		int n1 = 3;
+		int n2 = 4;
+		for(int i = 0 ; i < n2 ; i++) {
+			System.out.print(n.charAt(i));
+			
+		}
+		
+		
+	}
+}
+
+
